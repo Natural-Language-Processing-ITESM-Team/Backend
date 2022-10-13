899598e51351c9f0c3727d8e4cd175c5f89633c1
luisferro2 <a01378248@itesm.mx>
2022-10-13 18:49:50 -0500

    Add simple server in python to connect with AWS transcribe and AWS S3

---
+++ server.py
@@ -0,0 +1,70 @@
+from flask import request
+from flask import Flask
+import boto3
+import json
+from flask_cors import CORS
+from transcribe_test import transcribe_file
+from dotenv import load_dotenv
+import os
+
+load_dotenv("secrets.env")
+
+acces_key = os.getenv("AWS_ACCESS_KEY_ID")
+secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+session_token = os.getenv("AWS_SESSION_TOKEN")
+region = os.getenv("REGION_NAME")
+
+authenticated_client = boto3.client(
+        "s3",
+        aws_access_key_id=acces_key,
+        aws_secret_access_key=secret_access_key,
+        aws_session_token=session_token,
+        region_name=region
+)
+
+transcribe_client = boto3.client('transcribe',
+        aws_access_key_id=acces_key,
+        aws_secret_access_key=secret_access_key,
+        aws_session_token=session_token,
+        region_name=region)
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/getTranscription', methods=['POST'])
+def getTranscription():
+    global authenticated_client
+    
+    # The way to get my form fields
+    #request.form[]
+
+    file_key_json = request.get_json()
+
+    file_key = file_key_json["key"]
+
+    print(f"I server am going to ask for transcription for {file_key}")
+    
+    file_uri = "s3://buketa/" + file_key
+
+    print("I will place transcript in " + file_key[:-3] + "json")
+    transcribe_file('Example-job', file_uri, transcribe_client, file_key[:-3] + "json")
+
+    # store file in current folder.
+    authenticated_client.download_file("buketa", file_key[:-3] + "json", "helloback.json")
+
+    with open('helloback.json', 'r') as f:
+        json_data = json.load(f)
+
+    # boom bam bop, badabop boom POW return transcript
+    return json_data["results"]["transcripts"][0]["transcript"]
+
+    
+
+if __name__ == "__main__":
+    
+    app.run(host="0.0.0.0", port=8000)
---
+++ transcribe_test.py
@@ -0,0 +1,30 @@
+import time
+import boto3
+import urllib
+
+
+def transcribe_file(job_name, file_uri, transcribe_client, output_key):
+    transcribe_client.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': file_uri},
+        MediaFormat='wav',
+        LanguageCode='en-US',
+        OutputBucketName="buketa",
+        OutputKey=output_key
+    )
+
+    max_tries = 60
+    while max_tries > 0:
+        max_tries -= 1
+        job = transcribe_client.get_transcription_job(TranscriptionJobName=job_name)
+        job_status = job['TranscriptionJob']['TranscriptionJobStatus']
+        if job_status in ['COMPLETED', 'FAILED']:
+            print(f"Job {job_name} is {job_status}.")
+            if job_status == 'COMPLETED':
+                print(f"Transcript ready to be downloaded\n")
+                print(f"\t{job['TranscriptionJob']['Transcript']['TranscriptFileUri']}.")
+            break
+        else:
+            print(f"Waiting for {job_name}. Current status is {job_status}.")
+        time.sleep(10)
+
