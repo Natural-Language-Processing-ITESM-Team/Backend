366c354857c8a4af6bf216f31418404400db2586
luisferro2 <a01378248@itesm.mx>
2022-11-07 18:29:55 -0600

    script with database structure

---
+++ create-tables.py
@@ -0,0 +1,46 @@
+# import the mysql client for python
+import pymysql
+
+db_connection   = pymysql.connect( \
+    host="benchmarksdb.cn5bfishmmmb.us-east-1.rds.amazonaws.com", 
+    user="admin", password="vpcOwnChunkCloud", db="Ultron")
+
+db_cursor = db_connection.cursor()
+
+db_cursor.execute( \
+    """
+    CREATE TABLE Metrics(metricId INT NOT NULL PRIMARY KEY,
+                         name VARCHAR(50) NOT NULL,
+                         unit VARCHAR(50) NOT NULL)    
+                               
+    CREATE TABLE TTSServices(TTSServiceId INT NOT NULL PRIMARY KEY,
+                             name VARCHAR(50) NOT NULL)
+
+    CREATE TABLE STTServices(STTServiceId INT NOT NULL PRIMARY KEY,
+                             name VARCHAR(50) NOT NULL)
+
+    CREATE TABLE STTBenchmarks(metricId INT NOT NULL PRIMARY KEY,
+                               STTServiceId INT NOT NULL PRIMARY KEY,
+                               benchmarkValue FLOAT NOT NULL,
+                               FOREIGN KEY (metricId)
+                                   REFERENCES Metrics(metricId)
+                                   ON UPDATE CASCADE ON DELETE RESTRICT,
+                               FOREIGN KEY (STTServiceId)
+                                   REFERENCES STTServices(STTServiceId)
+                                   ON UPDATE CASCADE ON DELETE RESTRICT)  
+
+    CREATE TABLE TTSBenchmarks(metricId INT NOT NULL PRIMARY KEY,
+                               TTSServiceId INT NOT NULL PRIMARY KEY,
+                               benchmarkValue FLOAT NOT NULL,
+                               FOREIGN KEY (metricId)
+                                   REFERENCES Metrics(metricId)
+                                   ON UPDATE CASCADE ON DELETE RESTRICT,
+                               FOREIGN KEY (TTSServiceId)
+                                   REFERENCES TTSServices(TTSServiceId)
+                                   ON UPDATE CASCADE ON DELETE RESTRICT)                             
+    
+    """)
+
+
+
+# intermediate table after parent tables so that references exist.
