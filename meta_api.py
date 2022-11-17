e0576d8f2103d562387383e25cc7fe9d86d9e348
Rbn-Sanchez <A01378379@itesm.mx>
2022-11-17 12:13:44 -0600

    "meta api"

---
+++ meta_api.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv('secrets.env')
+
+WHATSAPP_NUMBER_ID = os.getenv('WHATSAPP_NUMBER_ID')
+WHATSAPP_ACCESS_TOKEN = os.getenv('WHATSAPP_ACCESS_TOKEN')
+PAGE_ACCESS_TOKEN = os.getenv('PAGE_ACCESS_TOKEN')
+
+def respondWhatsapp(clientNumber, message):
+    url = f'https://graph.facebook.com/v15.0/{WHATSAPP_NUMBER_ID}/messages'
+    headers = {'Authorization':f'Bearer {WHATSAPP_ACCESS_TOKEN}', 'Content-Type': 'application/json'}
+    json = {"messaging_product": "whatsapp", "to": str(clientNumber), "text": {"body" : str(message)}}
+
+    req = requests.post(url, headers=headers, json=json)
+    print(req.text)
+
+def respondMessenger(PSID,message):
+    url = f'https://graph.facebook.com/v15.0/me/messages?access_token={PAGE_ACCESS_TOKEN}'
+    json = {"messaging_type": "RESPONSE","recipient":{"id":str(PSID)},"message":{"text":message}}
+    
+    req = requests.post(url, json=json)
+    print(req.text)
