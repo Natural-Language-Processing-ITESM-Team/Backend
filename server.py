9b4db10524d0b846b4f350e3d4194759a360280a
luisferro2 <a01378248@itesm.mx>
2022-11-23 14:36:59 -0600

    Update server.py

--- server.py
+++ server.py
@@ -94,7 +94,7 @@ def choose_cloud_converse_back(client_query: str) -> str:
         # PROCESS FOR AMAZON LEX
         text_for_client = AWS.converse_back(client_query)
 
-    if text_for_client == "gracias por tu preferencia":
+    if text_for_client == "muchas gracias por tu preferencia":
         active_bot = False
 
     return text_for_client
