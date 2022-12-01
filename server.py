00db9a72336f5ed7ed66e3b7006260a64eb3ecdf
luisferro2 <a01378248@itesm.mx>
2022-11-30 20:03:09 -0600

    Update server.py

--- server.py
+++ server.py
@@ -409,8 +409,8 @@ def getTranscription():
         headers = {'Content-Type': 'application/json'}
         json = {"data": {"audio_response_link": f"https://buketa.s3.amazonaws.com/{file_key}"}}
         req = requests.post(url, headers=headers, json=json)
-        response_map = json.loads(req)
-        weird_transcript = response_map["transcripcion"]
+        #response_map = json.loads(req)
+        weird_transcript = req.text["transcripcion"]
         transcript = unicodedata.normalize("NFKD", weird_transcript).encode("ascii", "ignore")
         confidence = 0.5
 
