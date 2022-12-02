8a5d145b7ecb1559838f9070b7ccb66c1d1d7af3
luisferro2 <a01378248@itesm.mx>
2022-12-01 18:32:33 -0600

    Update server.py

--- server.py
+++ server.py
@@ -447,7 +447,7 @@ def getTranscription():
     elif best_stt_service == "AzureSTT":
         transcript, confidence = azure_transcribe_audio_file(file_key)
     elif best_stt_service == "NvidiaSTT":
-        url = 'https://709f-148-241-64-15.ngrok.io/Nvidia'
+        url = 'https://1a80-148-241-64-15.ngrok.io/Nvidia'
         headers = {'Content-Type': 'application/json'}
         json_info = {"data": {"audio_response_link": f"https://buketa.s3.amazonaws.com/{file_key}"}}
         req = requests.post(url, headers=headers, json=json_info)
